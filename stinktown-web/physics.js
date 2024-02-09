a2f4e9694115abd672a53af61ac8ac353c162e3e
Tralion <68718850+trevorbollinger@users.noreply.github.com>
2024-02-09 15:10:13 -0600

    init

--- stinktown-web/physics.js
+++
@@ -1,9 +0,0 @@
-function physics(){
-	
-}
-
-function collide(o1,o2){ //collision detection
-	if(o1.x+o1.width-o1.interactionDistance > o2.x && o1.y+o1.height-o1.interactionDistance > o2.y && o1.y<o2.y+o2.height+o1.interactionDistance && o1.x<o2.x+o2.width+o1.interactionDistance){
-		return true;
-	}
-}
