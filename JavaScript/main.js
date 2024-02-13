a276826d9306ae87885826fcc10df3e7a169101d
Tralion <68718850+trevorbollinger@users.noreply.github.com>
2024-02-13 13:49:44 -0600

    excel -> gsheets

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -11,10 +11,45 @@ class Card {
     }
 }
 
-fetch('data/cards.xlsx')
-.then(response => response.arrayBuffer())
+
+//LOCAL EXCEL FILE
+// fetch('data/cards.xlsx')
+// .then(response => response.arrayBuffer())
+// .then(data => {
+//     var workbook = XLSX.read(new Uint8Array(data), {type: 'array'});
+//     var sheet_name_list = workbook.SheetNames;
+//     var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+
+//     cds = []; //original object array
+
+//     xlData.forEach(function(row) {
+//         //insert row.text into objects
+//         cds.push(new Card(row.text, row.part, row.stinky));
+//     });
+
+//     shuffle(cds);
+//     lng = cds.length; //init length
+
+//     //clone origin array
+//     CARDS = cds; //Static array, allow repeats
+//     cardsu = cds; //Unique cards array, deny repeats
+
+//     var listItems = "";
+//     for (var i = 0; i < cds.length; i++) {
+//         listItems += "<li><a href='#'>" + cds[i].text + "</a></li>";
+//     }
+
+//     var ul = document.getElementById('myUL');
+//     ul.innerHTML += listItems;
+// })
+// .catch(error => console.error('Error:', error));
+
+
+//GOOGLE SHEETS FILE
+fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vQHsbDxDbKjhJZiTr_0l7Khb-Tn22VOO3aTBfoVdmAgT-bzP-l_2DOAnqSWabhXDjw90AT8L9fVURrj/pub?gid=817858363&single=true&output=csv')
+.then(response => response.text())
 .then(data => {
-    var workbook = XLSX.read(new Uint8Array(data), {type: 'array'});
+    var workbook = XLSX.read(data, {type: 'binary'});
     var sheet_name_list = workbook.SheetNames;
     var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
 
