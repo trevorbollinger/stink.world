611df8d7eb66fe00db2617a17992a2401b0614e4
Tralion <68718850+trevorbollinger@users.noreply.github.com>
2024-03-14 16:00:59 -0500

    variable card supoport

--- JavaScript/func.js
+++ JavaScript/func.js
@@ -1,90 +1,149 @@
- function toggleCollapsibleSection() {
-      var section = document.getElementById("myCollapsibleSection");
-      if (section.style.display === "none") {
-        section.style.display = "block";
-      } else {
-        section.style.display = "none";
-      }
-    }
+function toggleCollapsibleSection() {
+  var section = document.getElementById("myCollapsibleSection");
+  if (section.style.display === "none") {
+    section.style.display = "block";
+  } else {
+    section.style.display = "none";
+  }
+}
 
 function filterList() {
   cardsu = CDS;
   allowRepeats = repeatCB.checked;
   stinkyCheck = stinkyCB.checked;
 
-  if (stinkyCheck) cardsu = cardsu.filter(card => !card.stinky);
-  if (!allowRepeats) cardsu = cardsu.filter(card => !card.drawn);
+  if (stinkyCheck) cardsu = cardsu.filter((card) => !card.stinky);
+  if (!allowRepeats) cardsu = cardsu.filter((card) => !card.drawn);
 
   updaetDisplay();
 }
 
-function resetCards(){
+function resetCards() {
   cardsu = CDS;
   updaetDisplay();
 }
 
-function newCard(){
+function drawCards() {
   var display = document.getElementById("display");
-  var textDisplay = document.getElementById("textDisplay");
-  var letterDisplay = document.getElementById("letterDisplay");
+  var numDivs = parseInt(document.getElementById("numDivs").value);
 
   display.style.display = "block";
+  display.innerHTML = ""; // Clear previous divs
 
-  //r = 0;
-  r = randNum(cardsu.length);
-  selectedCard = cardsu[r];
-  selectedCard.drawn = true;
+  for (var i = 1; i <= numDivs; i++) {
+    // Dynamically create card elements
+    var card = document.createElement("div");
+    var arrowContainer = document.createElement("div");
+    var upArrow = document.createElement("span");
+    var downArrow = document.createElement("span");
+    var displayContainer = document.createElement("div");
+    var textDisplay = document.createElement("h3");
+    var letterDisplay = document.createElement("h5");
 
-  //update display
-  textDisplay.innerHTML = selectedCard.text;
-  letterDisplay.innerHTML = "Letter: " + selectedCard.letter;
-    console.log(cardsu[r].drawn);
+    // Set class names for elements
+    card.className = "card";
+    arrowContainer.className = "arrow-container";
+    upArrow.className = "arrow";
+    downArrow.className = "arrow";
+    displayContainer.className = "display-container";
+    textDisplay.className = "textDisplay";
+    letterDisplay.className = "letterDisplay";
 
-  filterList();
+    // Set arrow text content
+    upArrow.textContent = "↑";
+    downArrow.textContent = "↓";
+
+    // Set arrow onclick handlers
+    upArrow.onclick = createMoveUpFunction(card);
+    downArrow.onclick = createMoveDownFunction(card);
+
+    // Select card
+    var r = randNum(cardsu.length);
+    var selectedCard = cardsu[r];
+    selectedCard.drawn = true;
+    filterList();
+
+    // Set text display content
+    textDisplay.innerHTML = selectedCard.text;
+
+    // Set letter display content
+    letterDisplay.innerHTML = "Letter: " + selectedCard.letter;
+
+    // Append elements to their containers
+    arrowContainer.appendChild(upArrow);
+    arrowContainer.appendChild(downArrow);
+    displayContainer.appendChild(textDisplay);
+    displayContainer.appendChild(letterDisplay);
+
+    // Append containers to card
+    card.appendChild(arrowContainer);
+    card.appendChild(displayContainer);
+
+    // Append card to display
+    display.appendChild(card);
+  }
+
   updaetDisplay();
 }
-function randNum(max){
-  return Math.floor((Math.random() * max));
-}
-function updaetDisplay(){
-    
-    var listItems = "";
-    for (var i = 0; i < cardsu.length; i++) {
-        listItems += "<li><a href='#'>" + cardsu[i].text + "</a></li>";
-    }
 
-    var ul = document.getElementById('myUL');
-    ul.innerHTML = "";
-    ul.innerHTML += listItems;
- 
+function createMoveUpFunction(item) {
+  return function () {
+    if (item.previousElementSibling) {
+      item.parentNode.insertBefore(item, item.previousElementSibling);
+    }
+  };
+}
+
+function createMoveDownFunction(item) {
+  return function () {
+    if (item.nextElementSibling) {
+      item.parentNode.insertBefore(item.nextElementSibling, item);
+    }
+  };
+}
+
+function randNum(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function updaetDisplay() {
+  var listItems = "";
+  for (var i = 0; i < cardsu.length; i++) {
+    listItems += "<li><a href='#'>" + cardsu[i].text + "</a></li>";
+  }
+
+  var ul = document.getElementById("myUL");
+  ul.innerHTML = "";
+  ul.innerHTML += listItems;
 }
 
 function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
+  let currentIndex = array.length,
+    randomIndex;
 
   // While there remain elements to shuffle.
   while (currentIndex > 0) {
-
     // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
     // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
 
   return array;
 }
 
-
 function searchList() {
   // Declare variables
   var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById('myInput');
+  input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName('li');
+  li = ul.getElementsByTagName("li");
 
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < li.length; i++) {
